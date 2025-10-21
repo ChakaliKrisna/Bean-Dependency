6457a61cf25025946a22da53221308554917b8ae
Chakali_Krishna <manyamkrishna925@gmail.com>
2025-10-21 11:04:34 +0530

    initail commit

---
+++ src/main/java/bean_dependency/StarterApp.java
@@ -0,0 +1,34 @@
+package bean_dependency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.context.WebApplicationContext;
+
+//import hellospringbootapp.StarterApp;
+
+//import hellospringbootapp.autoconfig.SpringBootCourse;32
+
+
+@SpringBootApplication
+public class StarterApp  {
+	
+	public static void main(String[] args) {
+SpringApplication springapplication =new SpringApplication(Appconfig.class);
+		
+		ConfigurableApplicationContext container = springapplication.run(args);
+		
+//		AnnotationConfigApplicationContext container1=new AnnotationConfigApplicationContext ();
+		System.out.println("hello");
+       
+    }
+
+	
+	
+}
